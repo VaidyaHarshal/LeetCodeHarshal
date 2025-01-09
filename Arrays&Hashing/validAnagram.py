6d59b7c676673c46ba730b1f62a878afc93a06bd
Harshal Vaidya <92479500+VaidyaHarshal@users.noreply.github.com>
2025-01-08 20:58:40 -0500

    valid anagram
    Given two strings s and t, return true if the two strings are anagrams of each other, otherwise return false.

---
+++ Arrays&Hashing/validAnagram.py
@@ -0,0 +1,36 @@
+# Valid Anagram
+# Solved 
+# Given two strings s and t, return true if the two strings are anagrams of each other, otherwise return false.
+
+# An anagram is a string that contains the exact same characters as another string, but the order of the characters can be different.
+
+# Example 1:
+
+# Input: s = "racecar", t = "carrace"
+
+# Output: true
+# Example 2:
+
+# Input: s = "jar", t = "jam"
+
+# Output: false
+# Constraints:
+
+# s and t consist of lowercase English letters.
+
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+      if len(s) != len(t):
+        return False
+        
+      countS, countT = {}, {}
+      
+      for i in range(len(s)):
+        countS[i] = 1 + countS.get(s[i], 0)
+        countT[i] = 1 + countT.get(t[i], 0)
+
+      for j in countS:
+        if countS[j] != countT.get(j, 0):
+          return False
+          
+      return True
